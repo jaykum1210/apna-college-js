1688f0994753c0c1caa39e9bb2e46586e4f7d4fb
jaykum1210 <jaykumawat1210@gmail.com>
2025-10-06 19:42:45 +0530

    add files

---
+++ third.js
@@ -0,0 +1,36 @@
+// let a = "jay kumawat";
+// console.log(a[10]);
+
+// let b = `hello my name is jay kumawat`;
+// console.log(b);
+
+// let c = {
+//     item : "pen",
+//     price : 100,
+// };
+
+// console.log("Item is",c.item, "and its price is ",c.price);
+// console.log("The item is "+c.item + " and its price is "+ c.price)
+// console.log(`The item is ${c.item} and it's price is ${c.price}`);
+
+// console.log("Hello my name is\njay kumawat");
+
+// console.log(b.toUpperCase());
+// console.log(b.toLowerCase());
+// console.log(b.trim());
+
+// let d = "hello my name is Jay";
+// console.log(d.slice(0,10));
+
+// let e = "hello ";
+// let f = "jay";
+// console.log(e.concat(f));
+
+// console.log(d.replace("Jay", "Jayesh"));
+// console.log(d.replace("l","$"));
+// console.log(d.replaceAll("l","$"));
+
+// console.log(d.charAt(0));
+
+let g = prompt("Enter your full name");
+console.log(`@${g}${g.length}`);
