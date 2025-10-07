70022b85af46e6a186a2162f9c3d50668448eb34
jaykum1210 <jaykumawat1210@gmail.com>
2025-10-07 09:08:29 +0530

    add file

---
+++ sixth.js
@@ -0,0 +1,43 @@
+// alert("hello");
+// console.log("Hello");
+
+// console.log(document.body);
+// document.body.style.background = "green";
+// document.body.childNodes[7].innerText = "hii";
+
+// console.log(document.getElementById("heading"));
+// console.log(document.getElementsByClassName("head2"));
+
+// console.log(document.getElementsByTagName("p"))
+
+// console.log(document.querySelector("p"));
+// console.log(document.querySelectorAll("p"));
+
+// console.log(document.querySelectorAll(".head2"));
+// console.log(document.querySelector("#heading"));
+
+// let first = document.querySelector("button");
+// console.log(first);
+// console.log(first.tagName);
+
+// let secon = document.querySelector(".head2");
+// console.log(secon.tagName);
+
+// let third = document.querySelector("p");
+// console.log(third);
+
+// console.log(document.querySelector("div"));
+// console.log(document.querySelector("div").children);
+
+// let doc = document.querySelector("div");
+// console.log(doc);
+// // doc.innerText;
+// // doc.innerHTML;
+// doc.innerText = "bye";
+// doc.innerHTML = "<b>Hellojiji</b>"
+
+
+let a = document.body.innerHTML = "<h2>Hello JavaScript</h2>";
+let h2 = document.querySelector("h2");
+// console.log(h2.innerText);
+h2.innerText = h2.innerText + " from apna college";
